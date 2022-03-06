697648a508bc9f887ad98f7fb0ef6ed4b50e5dc4
namvdhe151370 <97592724+namvdhe151370@users.noreply.github.com>
2022-03-06 23:30:45 +0700

    login

---
+++ webThuoc/src/java/dao/BaseDAO.java
@@ -0,0 +1,24 @@
+
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public abstract class BaseDAO<T> {
+    protected Connection connection;
+    public BaseDAO()
+    {
+        try {
+            String user = "sa";
+            String pass = "123456";
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=Webbanthuoc";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(BaseDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
